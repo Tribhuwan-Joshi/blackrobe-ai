5c77a8a258d2da23a17e15b9cf3e160766698c47
Tribhuwan <mathincosmos@gmail.com>
2024-03-03 15:54:28 +0530

    Set user input state

--- app/home/ChatInput.tsx
+++ app/home/ChatInput.tsx
@@ -1,10 +1,24 @@
+"use client";
+import { KeyboardEvent, useState } from "react";
 import { BsSend } from "react-icons/bs";
 
 const ChatInput = () => {
+  const [userInput, setUserInput] = useState("");
+  const handleKeyDown = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+    console.log("keydown");
+    if (e.key == "Enter" && userInput.trim()) {
+      e.preventDefault();
+      console.log("user's choice", userInput.trim());
+      setUserInput("");
+    }
+  };
   return (
-    <div className="w-[90%] p-2 rounded-md absolute bg-[#8c40ef62]  bottom-4  flex-1 flex items-center justify-center">
+    <div className="w-[95%] md:w-[90%] p-2 rounded-md absolute bg-[#8c40ef62]  bottom-4  flex-1 flex items-center justify-center">
       {/* Replace input with textarea for multiline input */}
       <textarea
+        onChange={(e) => setUserInput(e.target.value)}
+        value={userInput}
+        onKeyDown={handleKeyDown}
         className="rounded-md min-h-max w-full bg-transparent  text-white text-lg placeholder:text-white placeholder:focus:text-gray-200 placeholder:text-base outline-none px-2 resize-none"
         autoFocus={true}
         placeholder="Chat With Blackrobe"
