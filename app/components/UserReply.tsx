44e40cd59d335d7b62420df2b3670a40b8b97fe2
Tribhuwan <mathincosmos@gmail.com>
2024-03-06 21:23:09 +0530

    create message object

--- app/components/UserReply.tsx
+++ app/components/UserReply.tsx
@@ -7,18 +7,18 @@ const UserReply = async ({ text, key }: { text: string; key?: number }) => {
   const session = await getServerSession(authOptions);
 
   return (
-    <div className="user-reply self-end md:mr-8 items-center  flex   gap-4 text-lg bg-[#8638ebcb] p-2 max-w-max  md:w-[80%]  rounded-md text-white ">
+    <div className="user-reply self-end md:mr-8 items-center  flex pr-6  gap-4 text-lg bg-[#8638ebcb] p-2 max-w-max  md:w-[80%]  rounded-md text-white ">
       {session?.user?.image ? (
         <Image
-          width={36}
-          height={36}
+          width={34}
+          height={34}
           className="rounded-full"
           src={session.user.image}
           alt="pfp"
         />
       ) : (
         <Image
-          width={36}
+          width={34}
           className="bg-white rounded-full"
           src={pfp}
           alt="default pfp"
--- app/home/ChatContainer.tsx
+++ app/home/ChatContainer.tsx
@@ -11,17 +11,35 @@ type messageType = {
 };
 
 const ChatContainer = () => {
-  const [messages, setMessages] = useState<messageType[]>([]);
+  const [messages, setMessages] = useState<messageType[]>([
+    {
+      type: "Bot",
+      text: "Welcome to Blackrobe ! Your personalized contract generator.",
+    },
+    {
+      text: "Select contract ID to generate the contract",
+      type: "Bot",
+      options: [
+        "Non-Disclosure Agreement (NDA)",
+        "Service Agreement",
+        "Employment Contract",
+        "Freelance Contract",
+        "Sales Contract",
+        "Investor Agreement",
+        "Insurance Agreement",
+        "Lease Agreement",
+        "Joint Venture Agreement",
+        "Licensing Agreement",
+        "Mortgage Contract",
+        "Supply Agreement",
+        "Maintenance Contract",
+        "Outsourcing Agreement",
+        "Agency Agreement",
+      ],
+    },
+    { type: "User", text: "Mortgage Contract" },
+  ]);
 
-  // const renderMessage = () => {
-  //   return messages.map((m, i) => {
-  //     if (m.type == "Bot") return <BotReply key={i} text={m.text} />;
-  //     else if (m.type === "User") {
-  //       return <UserReply key={i} text={m.text} />;
-  //     }
-  //     return null;
-  //   });
-  // };
   return (
     <div className=" flex  overflow-auto  space-y-3 relative flex-col gap px-2 py-4 mx-auto rounded-lg w-full sm:w-2/3 h-[95%] border border-white bg-[#2d2d2d56] bg-center ">
       <div className="chat-container space-y-3 p-2 flex-1 flex flex-col">
