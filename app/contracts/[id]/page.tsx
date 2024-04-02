18b7ea59d25d40cacc3b23be2f9e70d8d73cc076
Tribhuwan <mathincosmos@gmail.com>
2024-04-02 22:31:01 +0530

    fix contract style

--- app/contracts/[id]/page.tsx
+++ app/contracts/[id]/page.tsx
@@ -13,7 +13,7 @@ const page = async ({ params }: { params: { id: string } }) => {
   const contract = await getContract();
   if (!contract) notFound();
   return (
-    <div className="p-5 md:p-10 text-lg  text-gray-200 bg-gradient-to-r from-purple-900 via-purple-700 to-indigo-900">
+    <div className="p-5 md:p-10 text-lg  text-gray-200 bg-gradient-to-r from-purple-900 via-purple-700 to-indigo-700">
       <div className="w-[80%] mx-auto">
         <div className="flex items-center flex-row gap-4">
           <DeleteBtn contractId={params.id} />
