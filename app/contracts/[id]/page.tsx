b00d73d229da5a089f018ca8856f76c24c0ce42d
tjsm <mathincosmos@gmail.com>
2024-05-03 16:42:21 +0530

    allow download feature

--- app/contracts/[id]/page.tsx
+++ app/contracts/[id]/page.tsx
@@ -2,6 +2,7 @@ import prisma from "@/prisma/index";
 import DeleteBtn from "./DeleteBtn";
 import { notFound } from "next/navigation";
 import HomeBtn from "./HomeBtn";
+import DownloadPDF from "./DownloadPDF";
 
 const page = async ({ params }: { params: { id: string } }) => {
   const getContract = async () => {
@@ -10,22 +11,18 @@ const page = async ({ params }: { params: { id: string } }) => {
     });
     return contract;
   };
+
   const contract = await getContract();
   if (!contract) notFound();
+
   return (
-    <div className="p-5 md:p-10 text-lg  text-gray-200 bg-gradient-to-r from-purple-900 via-purple-700 to-indigo-700">
+    <div className="p-5 md:p-10 text-lg text-gray-200 bg-gradient-to-r from-purple-900 via-purple-700 to-indigo-700">
       <div className="w-[80%] mx-auto">
         <div className="flex items-center flex-row gap-4">
           <DeleteBtn contractId={params.id} />
           <HomeBtn />
         </div>
-
-        <div className=" border  border-gray-200 p-2 my-4 rounded-md">
-          <h1 className="text-center text-3xl font-semibold">
-            {contract?.title ?? "Contract"}
-          </h1>
-          <div dangerouslySetInnerHTML={{ __html: contract!.content }} />
-        </div>
+        <DownloadPDF contract={contract} />
       </div>
     </div>
   );
